544cb047d3d61807f90f8f74773031165170f56a
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-24 18:22:07 +0100

    add TaskList

---
+++ src/TasksList.tsx
@@ -0,0 +1,18 @@
+
+
+export const TasksList = () => {
+    return (
+        <ul>
+            <li>
+                <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
+            </li>
+            <li>
+                <input type="checkbox" checked={true}/> <span>JS</span>
+            </li>
+            <li>
+                <input type="checkbox" checked={false}/> <span>React</span>
+            </li>
+        </ul>
+    );
+};
+
