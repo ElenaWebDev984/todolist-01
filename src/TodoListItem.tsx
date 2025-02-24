f05c44de3b7013a967ecd448dc97dc02adaa47d6
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-24 18:22:48 +0100

    add components

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,30 +1,18 @@
+import {TodoListTitle} from "./TodoListTitle.tsx";
+import {AddTaskForm} from "./AddTaskForm.tsx";
+import {TasksList} from "./TasksList.tsx";
+import {FilterButtons} from "./FilterButtons.tsx";
 
 
 export const TodoListItem = () => {
     return (
         <div>
-            <h3>What to learn</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li>
-                    <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={true}/> <span>JS</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={false}/> <span>React</span>
-                </li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
+            <TodoListTitle />
+            <AddTaskForm />
+            <TasksList />
+            <FilterButtons />
         </div>
     );
 };
+
 
