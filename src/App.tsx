61e26e8c41922006db98d213cf15cc24e0e4cb53
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-03 14:21:47 +0100

    add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,14 @@ import {TaskType, TodoListItem} from "./TodoListItem.tsx";
 export const App = () => {
     // BLL
     const todoListTitle_1 = 'What to learn'
-    const todoListTitle_2 = 'What to buy'
 
     const task_1: TaskType[] = [
-        {id: 1, title: 'HTML', isDone: true},
-        {id: 2, title: 'CSS', isDone: true},
-        {id: 3, title: 'JS/TS', isDone: false},
-    ]
-    const task_2: Array<TaskType> = [
-        {id: 5, title: 'apple', isDone: true},
-        {id: 6, title: 'meat', isDone: true},
-        {id: 7, title: 'wine', isDone: false},
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false},
+        {id: 4, title: 'Redux', isDone: false},
+        {id: 5, title: 'Typescript', isDone: false},
+        {id: 6, title: 'RTK query', isDone: false},
     ]
 
     // TaskType[] === Array<TaskType>
@@ -23,7 +20,6 @@ export const App = () => {
     return (
         <div className={'app'}>
             <TodoListItem title={todoListTitle_1} tasks={task_1}  />
-            <TodoListItem title={todoListTitle_2} tasks={task_2} />
         </div>
     )
 }
